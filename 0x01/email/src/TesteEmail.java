3a4518f07129642a5df1deaf78f7cf41fcc7491a
Anderson Pazin <a.pazin@gmail.com>
2025-02-11 21:22:19 -0300

    Add files via upload

--- 0x01/email/src/TesteEmail.java
+++ 0x01/email/src/TesteEmail.java
@@ -16,6 +16,7 @@ public class TesteEmail {
 
     @Test
     void testar_email_mais_50_caracteres() {
-        assertFalse(Pessoa.emailValid("email_teste_muito_longo_nao_deve_ser_valido@dominio.com.br"));
+        String emailLongo = "email_teste_muito_longo_nao_deve_ser_valido@dominio.com.br";
+        assertEquals(false, Pessoa.emailValid(emailLongo));
     }
 }
